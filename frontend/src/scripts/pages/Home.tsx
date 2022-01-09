dcaa490c788b0381e3f24ea1da7b7465845f204d
Digivore <pierre@digivore.fr>
2022-01-09 17:56:00 +0100

    Update typo, color, pictures and add text

--- frontend/src/scripts/pages/Home.tsx
+++ frontend/src/scripts/pages/Home.tsx
@@ -15,14 +15,19 @@ export default function Home(props: InferProps<typeof propTypes>): JSX.Element {
     <div data-layout="--grid">
       <div className="wrap wrap--fit-screen--with-background" data-layout="--flex--justify-center--items-center">
         <div className="image">
-          <img src="static/images/home2.jpg" alt="Digivore background" width="100%" />
+          <img src="static/images/home3.jpg" alt="Digivore background" width="100%" />
         </div>
-        <h1>Digivore</h1>
+        <h1>digivore</h1>
       </div>
       <div className="wrap wrap--padded---txt-center">
         <div className="container" data-layout="--grid" data-gap="--6">
-          <h2>Fullstack Developer</h2>
-          <p>Team spirit - Autonomous  - Demanding - Curious</p>
+          <div data-layout="--grid--items-center" data-cols="--2" data-gap="--4">
+            <img src="static/images/portrait.jpg" alt="Pierre Bonnin" />
+            <div data-layout="--grid" data-gap="--2">
+              <h2>Fullstack Developer</h2>
+              <p>Team spirit - Autonomous  - Demanding - Curious</p>
+            </div>
+          </div>
           <div data-layout="--grid" data-cols="--2" data-hgap="--4">
             <div className="skills" data-layout="--flex--justify-center">
               <ul data-layout="--grid" data-gap="--5">
@@ -79,6 +84,28 @@ export default function Home(props: InferProps<typeof propTypes>): JSX.Element {
                     <span className="dot" />
                     Toulouse
                   </p>
+                  <h4>Software engineer</h4>
+                  <p className="sumup">"Startup adventure" in a US company targeting the US insurance market</p>
+                  <ul>
+                    <li>B2b & b2c apps design and fullstack development (node, react, scss)</li>
+                    <li>Euthereum & Polygon smart contract development + NFT minting</li>
+                  </ul>
+                </li>
+                <li>
+                  <p>
+                    {'2017 > 2020'}
+                    <span className="dot" />
+                    Horizon
+                    <span className="dot" />
+                    Toulouse
+                  </p>
+                  <h4>CTO</h4>
+                  <p className="sumup">Managing the company's technical pole and ensuring production workflow</p>
+                  <ul>
+                    <li>Scheduling, production follow-up, recruiting, management and team upskill</li>
+                    <li>Forecasting needs, R&D projects supervision, IT infrastructuren development and publishing</li>
+                    <li>Customers management (teaching, products evolutions, litigations)</li>
+                  </ul>
                 </li>
               </ul>
             </div>
